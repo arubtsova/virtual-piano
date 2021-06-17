7c5952fd509bdac1985302c2e6b2975af6b58996
Рубцова Анастасия Витальевна <arubtsova66@gmail.com>
2021-06-17 11:19:13 +0300

    changed blocks

--- index.js
+++
@@ -1,3 +0,0 @@
-'use strict'
-
-let buttonDo = document.querySelector('key-do');
---
+++ script.js
@@ -0,0 +1,23 @@
+'use strict'
+
+// let buttonDo = document.getElementById('button-white');
+//     buttonDo.onclick = function() {
+//         if (buttonDo.style.backgroundColor == 'black') {
+//             buttonDo.style.backgroundColor = 'white'
+//             buttonDo.style.color = 'black';
+//         } else {
+//             buttonDo.style.backgroundColor = 'black';
+//             buttonDo.style.color = 'white';
+//         }
+//     }
+
+//     let flashingElem = document.getElementById('header__piano');
+//     setInterval(() => flashingElem.hidden = !flashingElem.hidden, 3000);
+
+
+//     let div = document.createElement('div');
+//     div.className = "alert";
+//     div.innerHTML = "<strong>Всем привет!</strong> Вы прочитали важное сообщение.";
+
+//     document.body.append(div);
+//     setTimeout(() => div.remove(), 3000);
