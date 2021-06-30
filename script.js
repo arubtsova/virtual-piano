5909e6f3509d485a21015b4dcda85759d5c16ba1
Рубцова Анастасия Витальевна <arubtsova66@gmail.com>
2021-06-30 13:18:54 +0300

    add selectors

--- script.js
+++ script.js
@@ -21,3 +21,18 @@
 
 //     document.body.append(div);
 //     setTimeout(() => div.remove(), 3000);
+
+
+
+let keys = document.querySelectorAll('.key');
+let nowplaying = document.querySelectorAll('.nowplaying');
+let hints = document.querySelectorAll('.hints');
+
+function playingNote(play){
+    let music = document.querySelector(`audio[data-key='${play.keyCode}']`);
+    let key = document.querySelector(`.key[data-key='${play.keyCode}']`);
+    if (!key){
+        return;
+    }
+    let NameNote = key.getAttribute('')
+}
